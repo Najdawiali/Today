a2e93032695793a03b775d70533cba95f9c6c65f
ali <najdawiali369@gmail.com>
2023-09-09 11:36:29 +0300

    first

---
+++ Q2.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+
+X = ["Category A", "Category B", "Category C", "Category D"]
+Y = [10, 25, 15, 30]
+plt.bar(X, Y)
+plt.title("Scatter Plot Example")
+plt.xlabel("X-axes")
+plt.ylabel("Y-axes")
+
+plt.scatter([1, 2, 3, 4, 5], [5, 7, 4, 2, 8])
+plt.title("Scatter Plot Example")
+plt.show()
+
+
+
